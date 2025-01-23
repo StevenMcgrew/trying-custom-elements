57dc12c09e73b5875ee2fd92400626f5fac7ecc5
StevenMcgrew <stevenmcgrew8224@msn.com>
2025-01-22 20:10:49 -0800

    Fixed render and access order

--- index.js
+++ index.js
@@ -1,33 +1,76 @@
+HTMLElement.prototype.q = function (selector) {
+    return this.querySelector(selector);
+};
+
+HTMLElement.prototype.on = function (type, listener, options) {
+    this.addEventListener(type, listener, options);
+};
+
 class ModalBox extends HTMLElement {
     constructor() {
-        super()
+        super();
+        // With a <slot> tag use this code
+        const childHTML = this.innerHTML;
+        this.innerHTML = "";
+        const template = document.querySelector("template.modalbox-456-0");
+        this.append(template.content);
+        const childSlot = this.querySelector("modalbox-456 slot");
+        childSlot.innerHTML = childHTML;
     }
     connectedCallback() {
-        const childHTML = this.innerHTML
-        this.innerHTML = ""
 
-        const template = document.querySelector("template.modal-box");
-        this.append(template.content);
 
-        const childSlot = this.querySelector("modal-box slot");
-        childSlot.innerHTML = childHTML
     }
 }
-customElements.define("modal-box", ModalBox);
+customElements.define("modalbox-456", ModalBox);
 
 
 class Question extends HTMLElement {
+    static get observedAttributes() {
+        return ["text"];
+    }
     constructor() {
-        super()
-    }    connectedCallback() {
-        const childHTML = this.innerHTML
-        this.innerHTML = ""
-
-        const template = document.querySelector("template.ques-tion");
+        super();
+        // Without slot
+        const template = document.querySelector("template.question-321-0");
         this.append(template.content);
+    }
 
-        const childSlot = this.querySelector("ques-tion slot");
-        childSlot.innerHTML = childHTML
+    connectedCallback() {
+
+    }
+
+    attributeChangedCallback(name, oldValue, newValue) {
+        if (newValue === oldValue) {
+            return
+        }
+        this['set'+name](newValue)
+    }
+
+    setText(newVal) {
+        this.q("p").textContent = newVal
     }
 }
-customElements.define("ques-tion", Question);
+customElements.define("question-321", Question);
+
+// EXAMPLE of using attributes and properties "in sync"
+class MyCustomElement extends HTMLElement {
+    static get observedAttributes() {
+      return ['my-attribute'];
+    }
+  
+    attributeChangedCallback(name, oldValue, newValue) {
+      if (name === 'my-attribute') {
+        this.myProperty = newValue;
+      }
+    }
+  
+    get myProperty() {
+      return this._myProperty;
+    }
+  
+    set myProperty(value) {
+      this._myProperty = value;
+      this.setAttribute('my-attribute', value);
+    }
+  }
